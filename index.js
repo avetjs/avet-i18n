6e7f5ad9fda191efea8db1c6fffb22374c694eef
okoala <dapixp@gmail.com>
2017-11-07 15:43:33 +0800

    feat: update i18n

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { translate } from 'react-i18next';
-import i18n from './lib/i18n';
+import * as i18n from './lib/i18n';
 
 export default function(Component, locales, options) {
   const WrapComponent = translate(locales, Object.assign({}, {
